5a66c6b748c9340e2043d0f62c0b03849d0d5610
Abhisek Pattnaik <abhisekp@engineer.com>
2015-08-24 08:08:54 +0530

    Change variable name

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-function waitThreeSeconds() {
-  var ms = 3000 + new Date().getTime();
+/*
+ * This a wait three
+ * @param a Nothing
+ */
+function waitThreeSeconds(a) {
+  var millis = 3000 + new Date().getTime();
 
-  while(new Date() < ms) {
+  while (new Date() < millis) {
     console.log('loading... clicking anything on page, will be queued in event queue');
   }
   console.log('waited 3 seconds');
 }
 
 var clickTimes = 0;
+
 function clickHandler() {
   clickTimes++;
   console.log('something is clicked ' + clickTimes + ' time.');
 }
-
 
 document.addEventListener('click', clickHandler);
 
