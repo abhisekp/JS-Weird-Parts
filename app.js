91cffbb4d545145873faf821a7a91a7aa25b703c
Abhisek Pattnaik <abhisekp@engineer.com>
2015-08-29 21:49:42 +0530

    Demo dynamic array

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-var  z = {
-    zName: 'Tommy Hilfrangler',
-    zLog: function () {
-        var self = this;
+var arr = [
+    1,
+    true, {
+        name: 'Tom',
+        address: '589, Main St.'
+    },
+    function show(name) {
+        'use strict';
+        console.log(name);
+        console.log(this);
 
-        console.log(self);
-
-        function changeName(name) {
-            self.zName = name;
-        }
-
-        changeName('Murfy Mustaffa');
-        console.log(self);
+        console.log(this.name);
+        console.log(this[2].name);
     }
-};
+];
 
-console.log(z);
-
- z.zLog();
-
-var logger =  z.zLog;
-
-logger();
-
-console.log(self);
+arr.name = 'Maccau';
+//var name = 'Hilbert';
+arr[3](arr[2].name);
+console.log('----------');
+var fn = arr[3];
+fn(arr[2].name);
