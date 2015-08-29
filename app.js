109ab3b0c94d1fbf9bbc402dd36e9bca60d5b456
Abhisek Pattnaik <abhisekp@engineer.com>
2015-08-30 00:50:52 +0530

    Demo arguments and es6 spread operator

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
-var arr = [
-    1,
-    true, {
-        name: 'Tom',
-        address: '589, Main St.'
-    },
-    function show(name) {
-        'use strict';
-        console.log(name);
-        console.log(this);
+function greet(fname, lname, lang = 'en', ...langs) {
+    console.log(arguments);
+    console.log(langs);
+}
 
-        console.log(this.name);
-        console.log(this[2].name);
-    }
-];
-
-arr.name = 'Maccau';
-//var name = 'Hilbert';
-arr[3](arr[2].name);
-console.log('----------');
-var fn = arr[3];
-fn(arr[2].name);
+greet('Tommy', 'Hism', 'es', 'en', 'hi');
